e5dacee4464cf3a9d2cdfa7e4528c7dd497d1890
Peter Bollhorn <pbollhorn@gmail.com>
2025-05-18 14:56:59 +0200

    links are vertically stacked now

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,10 +2,18 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-      <nav id="navbar">
-        <Link to="/">Home</Link>
-        <Link to="/vision">Vision</Link>
-        <Link to="/endpoints">Endpoints</Link>
-      </nav>
+    <nav id="navbar">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/vision">Vision</Link>
+        </li>
+        <li>
+          <Link to="/endpoints">Endpoints</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
